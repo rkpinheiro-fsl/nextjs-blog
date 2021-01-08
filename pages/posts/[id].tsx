52adf46bad5a3fc38f5cb71514a8df61b364ce5b
Rurik Pinheiro <rurik@fullstacklabs.co>
2021-01-08 10:41:06 -0300

    feat: enable typescript

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -3,10 +3,20 @@ import { getAllPostIds, getPostData } from '../../lib/posts'
 import Head from 'next/head'
 import Date from '../../components/date'
 import utilStyles from '../../styles/utils.module.css'
+import { GetStaticProps, GetStaticPaths } from 'next'
 
-export default function Post({postData}) {
-  return (<Layout>
-  <Head>
+export default function Post({
+  postData
+}: {
+  postData: {
+    title: string
+    date: string
+    contentHtml: string
+  }
+}) {
+  return (
+    <Layout>
+      <Head>
         <title>{postData.title}</title>
       </Head>
       <article>
@@ -16,22 +26,23 @@ export default function Post({postData}) {
         </div>
         <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
       </article>
-</Layout>)
+    </Layout>
+  )
 }
 
-export async function getStaticPaths() {
-    const paths = getAllPostIds()
-    return {
-      paths,
-      fallback: false
-    }
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = getAllPostIds()
+  return {
+    paths,
+    fallback: false
   }
+}
 
-  export async function getStaticProps({ params }) {
-    const postData = await getPostData(params.id)
-    return {
-      props: {
-        postData
-      }
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const postData = await getPostData(params.id as string)
+  return {
+    props: {
+      postData
     }
   }
+}
